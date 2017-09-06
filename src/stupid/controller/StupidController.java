2b2efec87b2119aed84283abd922919e58915374
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-09-06 12:54:06 -0600

    Initial commit

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,10 @@
+package stupid.controller;
+
+public class StupidController
+{
+	
+	public void start()
+	{
+		System.out.print("not even salty");
+	}
+}
---
+++ src/stupid/model/ThirdStupid.java
@@ -0,0 +1,9 @@
+package stupid.model;
+
+public class ThirdStupid
+{
+	public static void main(String [ ] args)
+	{
+		
+	}
+}
